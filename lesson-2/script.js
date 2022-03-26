f723fd94dd2dac3c1235cca728be87657661511d
Son Tung <newstarvn01@gmail.com>
2022-03-26 20:36:46 +0700

    1

---
+++ lesson-2/script.js
@@ -0,0 +1,45 @@
+// // Link: https://gist.github.com/banx9x/0d487aa08e761e9dafa3025f3f9ffe4e
+// // bài 1: Tính BMI
+// let w = prompt("Nhập cân nặng của bạn: ");
+// let h = prompt("Nhập chiều cao của bạn: ");
+
+// function findBMI(w, h) {
+//   return w / h ** 2;
+// }
+
+// let BMI = findBMI(w, h).toFixed(2);
+
+// if (BMI <= 16) {
+//   console.log("Gầy độ III", bmi);
+// } else if (BMI <= 17.5) {
+//   console.log("Gầy độ II", BMI);
+// } else if (BMI <= 18.5) {
+//   console.log("Gầy độ I", BMI);
+// } else if (BMI <= 25) {
+//   console.log("Bình thường", BMI);
+// } else if (BMI <= 30) {
+//   console.log("Hơi béo", BMI);
+// } else if (BMI <= 35) {
+//   console.log("Béo phì độ I", BMI);
+// } else if (BMI <= 40) {
+//   console.log("Béo phì độ II", BMI);
+// } else if (40 < BMI) {
+//   console.log("Béo phì độ III", BMI);
+// }
+
+// // bài 2: Kiểm tra số lớn nhất
+// let a = prompt("Nhập số a: ");
+// let b = prompt("Nhập số b: ");
+// let c = prompt("Nhập số c: ");
+
+// function findMax(a, b, c) {
+//   let max = a;
+//   if (max < b) {
+//     max = b;
+//   }
+//   if (max < c) {
+//     max = c;
+//   }
+//   console.log("số lớn nhất là", max);
+// }
+// findMax(a,b,c)
